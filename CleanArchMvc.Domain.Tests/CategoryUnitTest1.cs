820a1f777ed7d86305f51299d99ac7d05fb773db
gabeFrancisco <gabegit2@gmail.com>
2025-06-11 16:44:44 -0300

    more tests to Category entity

--- CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -15,10 +15,20 @@ public class UnitTest1
     }
 
     [Fact]
-     public void CreateCategory_WithValidParameters_ResultObjectValidState1()
+    public void CreateCategory_WithNegativeId_ResultObjectValidState1()
     {
         Action action = () => new Category(1, "Wrong Cat name");
         action.Should()
             .NotThrow<DomainExceptionValidation>();
+            // .WithMessage("Invalid ID data!");
+    }
+
+    [Fact]
+    public void CreateCategory_MissingNameValue_DomainExceptionRequiredName()
+    {
+        Action action = () => new Category(0, "");
+        action.Should()
+            .Throw<DomainExceptionValidation>()
+            .WithMessage("Invalid name. Name is required!");
     }
 }
