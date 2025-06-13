c54ec88c65603bef9bb6ca43a7c4e849c80529bf
gabeFrancisco <gabegit2@gmail.com>
2025-06-13 15:25:43 -0300

    created CategoryRepository

---
+++ CleanArchMvc.Infra.Data/Identity/CategoryRepository.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArchMvc.Infra.Data.Identity
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Category> Create(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+             _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+                _context.Categories.Update(category);
+            await _context.SaveChangesAsync();
+
+            return category;
+        }
+    }
+}
